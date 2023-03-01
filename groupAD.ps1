72be43a7aadae52c344a9876540debe108dbf934
nshein1 <93554048+nshein1@users.noreply.github.com>
2023-03-01 14:20:35 -0500

    Add files via upload

---
+++ groupAD.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module activedirectory
+        
+        $users = Import-Csv c:\UserV2.csv
+
+foreach ($User in $users) 
+{
+    $group = $User.Group
+    $username = $User.User
+
+    if (Get-ADUser -Filter "SamAccountName -eq '$($username)'") {
+        Add-ADGroupMember `
+        -Identity $group `
+        -Members $username
+        Write-Host "$username added to $group"
+    }
+    else
+    {
+
+        Write-Warning "$username not found in Active Directory"
+    }
+}
---
+++ newAD.ps1
@@ -0,0 +1,34 @@
+﻿Import-Module activedirectory
+
+$ADUsers = Import-csv c:\betterPass.csv
+
+$UPN = "robotics.rome"
+
+foreach ($User in $ADUsers)
+{
+
+$Role = $User.Role
+$Firstname = $User.FirstName
+$Lastname = $User.LastName
+$Username = $User.AccountName
+$Password = $User.Password
+
+if (Get-ADUser -F {SamAccountName -eq $Username})
+{
+Write-Warning "User with username $Username already exists"
+}
+else
+{
+
+New-ADUser `
+    -SamAccountName $Username `
+    -UserPrincipalname "$Username@$UPN" `
+    -Name "$Firstname, $Lastname" `
+    -GivenName $firstname `
+    -Enabled $True `
+    -AccountPassword (ConvertTo-secureString $Password -AsPlainText -Force) -ChangePasswordAtLogon $False
+
+}
+}
+
+Read-Host -Prompt "Press Enter to exit"
